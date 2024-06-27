7716802d6d80f9cf22c1b18155874aece1e19ad2
朝阳 <oncwnuBhVpOmPmRq191AqovHmPjY@git.weixin.qq.com>
2024-06-27 09:56:23 +0800

    first commit

---
+++ src/components/Greet.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import { type } from '@tauri-apps/api/os'
+import { appWindow, PhysicalSize } from '@tauri-apps/api/window'
+import { hide, show } from '@tauri-apps/api/app'
+import { register, unregisterAll } from '@tauri-apps/api/globalShortcut'
+import { ref } from 'vue'
+import { invoke } from '@tauri-apps/api/tauri'
+import { fetch, type FetchOptions } from '@tauri-apps/api/http'
+const greetMsg = ref('')
+const name = ref('')
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsg.value = await invoke('greet', { name: name.value })
+}
+
+const windowFocused = ref(true)
+register('Ctrl+.', () => {
+  // 如果窗口已经显示，就隐藏
+  if (windowFocused.value) {
+    // 窗口打开时居中还是上次位置
+    // if (!isRememberPosition.value) {
+    //   appWindow.center()
+    // }
+    windowFocused.value = false
+    showWindow()
+  } else {
+    windowFocused.value = true
+    hideWindow()
+  }
+})
+
+/**
+ * 显示窗口
+ */
+const showWindow = async () => {
+  const platform = await type()
+  console.log(111)
+  appWindow.unminimize()
+
+  if (platform === 'Darwin') {
+    show()
+  } else {
+    appWindow.show()
+  }
+
+  appWindow.setFocus()
+}
+const fetchApi = async (url) => {
+  // const { data }: Record<string, any> = await fetch(url, {
+  //   ...options,
+  //   method,
+  //   timeout: 1000 * 60,
+  //   headers: {
+  //     ...headers,
+  //     'Content-Type': 'application/json',
+  //     'user-agent':
+  //       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+  //   }
+  // })
+  console.log(url,'url')
+  const data = await fetch(url)
+  console.log(data,'data')
+  const { error } = data
+
+  if (error) throw new Error(error.message)
+
+  return data
+}
+/**
+ * 隐藏窗口
+ */
+const hideWindow = async () => {
+  const platform = await type()
+
+  if (platform === 'Darwin') {
+    hide()
+  } else {
+    appWindow.hide()
+  }
+}
+const handleEnter = (v) => {
+  console.log(v.target.value, 'sakdaskdka')
+  fetchApi(`https://www.baidu.com/s?wd=${v.target.value}`).then((res) => {
+    console.log(res, 'res')
+  })
+}
+</script>
+
+<template>
+  <div class="container">
+    <form class="row" @submit.prevent="greet">
+      <a-input id="greet-input" v-model="name" autofocus @press-enter="handleEnter" placeholder="Enter a name..." />
+    </form>
+  </div>
+</template>
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+input {
+  padding: 10px;
+  width: 700px;
+  border-radius: 5px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+export default defineConfig(async () => ({
+  plugins: [vue(),
+    AutoImport({
+      resolvers: [ArcoResolver()],
+    }),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true
+        })
+      ]
+    })
+  ],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
